4363f1962c45474c3b5d55e1c89d64b30565fdbc
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-06-02 10:53:44 +0400

    api status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ Flask Application """
 from models import storage
@@ -11,7 +12,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
